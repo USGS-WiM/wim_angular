6e00811462236d31440b648f33f764f3d68cd01d
jknewson <jknewson@usgs.gov>
2015-04-27 13:24:09 -0600

    adding gulp and dist

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+﻿// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+
+// Concatenate & Minify JS
+gulp.task('scripts', function () {
+    return gulp.src('src/**/*.js')
+        .pipe(concat('wim_angular.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('wim_angular.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+
+// Default Task
+gulp.task('default', ['scripts']);
