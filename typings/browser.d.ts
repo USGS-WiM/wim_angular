874fe1c1108dd62e7cf2e74b99bf74cbd877dc4e
Draper <bdraper@usgs.gov>
2016-04-27 13:57:19 -0500

    added support for featureLayer legend in wimLegend;updated wimLegend example

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,2 +1,4 @@
-/// <reference path="browser\ambient\angular\angular.d.ts" />
-/// <reference path="browser\ambient\jquery\jquery.d.ts" />
+/// <reference path="browser/ambient/angular/angular.d.ts" />
+/// <reference path="browser/ambient/angular/index.d.ts" />
+/// <reference path="browser/ambient/jquery/index.d.ts" />
+/// <reference path="browser/ambient/jquery/jquery.d.ts" />
