28be50b95878354a303a3069ceba6cadfbeafcf4
Marty <marsmith@usgs.gov>
2017-08-11 15:02:40 -0400

    update wimLegend to allow for included legend image

--- dist/Directives/wimLegend.js
+++ dist/Directives/wimLegend.js
@@ -1,8 +1,13 @@
-var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-};
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var WiM;
 (function (WiM) {
     var Directives;
@@ -111,7 +116,8 @@ var WiM;
                 }
                 if (mlyr.type == "wms") {
                     mlyr.isOpen = true;
-                    mlyr.legendURL = mlyr.url + "?version=1.1.1&request=GetLegendGraphic&format=image/png&layer=" + mlyr.layerParams.layers;
+                    if (!mlyr.legendURL)
+                        mlyr.legendURL = mlyr.url + "?version=1.1.1&request=GetLegendGraphic&format=image/png&layer=" + mlyr.layerParams.layers;
                 }
             };
             wimLegendController.prototype.changeBaseLayer = function (key, evt) {
